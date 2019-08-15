0b8960e58b4630f6e27e2989a217a03ff8b45796
Anerco <aner.cohen@gmail.com>
2019-08-15 23:39:38 +0300

    added eslint  airbnb-base & prettier

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import http from "http";
 const app = express();
 const server = http.createServer(app);
 
-var host = process.env.HOST || "0.0.0.0";
-var port = process.env.PORT || 8080;
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 8080;
 
 server.on("listening", () => {
   console.log(`Server is running on ${host}:${port}`);
 });
 
 // Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -23,7 +23,7 @@ app.get("/check", (request, response) => {
 
 const onUncaughtException = err => {
   console.log(" UNCAUGHT EXCEPTION ");
-  console.log("[Inside 'uncaughtException' event] " + err.stack || err.message);
+  console.log(`[Inside 'uncaughtException' event] " ${err.stack || err.message}`);
 };
 
 process.on("uncaughtException", onUncaughtException.bind(null));
